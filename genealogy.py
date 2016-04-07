6da4ae39263f3e50c56b0575db489820a7fd932d
Jean Petitclerc <jean.petitclerc@groupepp.com>
2016-04-07 00:27:11 -0400

    Ajout de form pour le calcul de l'age. Plein de bugs. Flashed messages marchent pas, ... il est tard...

--- genealogy.py
+++ genealogy.py
@@ -1,12 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
+from flask_wtf import Form
+from wtforms import SubmitField, DateField
+from wtforms.validators import DataRequired
+
 from AgeAt import calc_age
 from datetime import date
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'BlaBlaBla'
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+
+
+class AgeAtForm(Form):
+    date_naissance = DateField('Date de naissance', format='%Y-%m-%d', validators=[DataRequired()])
+    date_deces = DateField('Date du décès', validators=[DataRequired()])
+    submit = SubmitField('Calculer')
 
 
 @app.route('/')
@@ -14,12 +25,23 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/AgeAt')
+@app.route('/AgeAt', methods=['GET', 'POST'])
 def calc_age_at():
-    date_naissance = date(1875, 5, 23)
-    date_deces = date(1913, 4, 15)
-    (years, months, days) = calc_age(date_naissance, date_deces)
-    return render_template('ageat.html', years=years, months=months, days=days)
+    years = None
+    months = None
+    days = None
+    form = AgeAtForm()
+    if form.validate_on_submit():
+        date_naissance = form.date_naissance.data
+        date_deces = form.date_deces.data
+        if date_deces < date_naissance:
+            print("Dates décès avant date de naissance.")
+            flash("La date du décès doit être après la date de naissance.")
+            years = -1
+        else:
+            (years, months, days) = calc_age(date_naissance, date_deces)
+        return render_template('ageat.html', form=form, years=years, months=months, days=days)
+    return render_template('ageat.html', form=form, years=None)
 
 
 @app.errorhandler(404)
